201f88a8a6bd76adbc2feb1af8864a9ca410af3d
anton-sales <anton.arus@gmail.com>
2018-09-24 15:45:51 +1200

    changed header look

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,20 +3,78 @@ import React from 'react'
 class Header extends React.Component {
   render () {
     return (
-      <section className='hero is-dark'>
-        <div className='hero-body'>
-          <div className='container'>
-            <h1 className='title'>
-              Lucky Travel
-            </h1>
-            <h2 className='subtitle'>
-              Where magic happens
-            </h2>
+      <nav className='navbar is-transparent'>
+        <div className='navbar-brand'>
+          <a className='navbar-item' href='https://bulma.io'>
+            <h1 className='title'>Flights</h1>
+          </a>
+          <div
+            className='navbar-burger burger'
+            data-target='navbarExampleTransparentExample'
+          >
+            <span />
+            <span />
+            <span />
           </div>
         </div>
-      </section>
+
+        <div id='navbarExampleTransparentExample' className='navbar-menu'>
+          <div className='navbar-start'>
+            <a className='navbar-item'>
+              Flights
+            </a>
+
+            <a className='navbar-item'>
+              Shuttle
+            </a>
+
+            <a className='navbar-item'>
+              Cruise
+            </a>
+
+          </div>
+        </div>
+
+        <div className='navbar-end'>
+          <div className='navbar-item'>
+            <div className='field is-grouped'>
+              <p className='control'>
+                <a className='bd-tw-button button'>
+                  <span>
+                    Log In
+                  </span>
+                </a>
+              </p>
+              <p className='control'>
+                <a className='button is-primary'>
+                  <span>Log Out</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </nav>
     )
   }
 }
 
 export default Header
+
+{
+  /* <section className='hero is-dark'>
+        <div className='hero-body'>
+          <div className='container'>
+            <div className='content'>
+              <span className='button has-addons is-right'>Log In</span>
+              <span className='button has-addons is-right'>Log Out</span>
+              <h1 className='title'>
+                Lucky Travel
+              </h1>
+              <h2 className='subtitle'>
+                Where magic happens
+              </h2>
+            </div>
+          </div>
+        </div>
+      </section> */
+}

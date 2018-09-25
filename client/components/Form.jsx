2897c9a8850c58f46bd4c165e7348b1c882595f9
anton-sales <anton.arus@gmail.com>
2018-09-25 16:00:38 +1200

    cleaned form code and got radio columns working yahooo

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -4,31 +4,50 @@ class Form extends React.Component {
   render () {
     return (
       <div className='container'>
-            <h1 className='title has-text-centered'>Book a Flight</h1>
+        <h1 className='title has-text-centered'>Book a Flight</h1>
         <div className='columns'>
-            <div className='column is-half'>
-              <label className='label is-medium'>From</label>
-              <div className='field'>
+          <div className='column is-half'>
+            <label className='label is-medium'>From</label>
+            <div className='field'>
+              <input
+                className='input is-medium'
+                placeholder='Airport or City'
+              />
+            </div>
+          </div>
 
-                <input
-                  className='input is-medium'
-                  placeholder='Airport or City'
-                />
-                <label class='radio'>
-                  <input type='radio' name='trip' />
-                  Return trip
-                </label>
-              </div>
+          <div className='column is-half'>
+            <label className='label is-medium'>From</label>
+            <div className='field'>
+              <input
+                className='input is-medium'
+                placeholder='Airport or City'
+              />
             </div>
-            <div className='column is-half'>
-              <label className='label is-medium'>From</label>
-              <div className='field'>
-                <input
-                  className='input is-medium'
-                  placeholder='Airport or City'
-                />
-              </div>
-            </div>
+          </div>
+        </div>
+
+        <div class='columns'>
+          <div className='column is-one-third'>
+            <label className='radio'>
+              <input type='radio' name='return-trip' />
+              Return Trip
+            </label>
+          </div>
+
+          <div className='column'>
+            <label className='radio'>
+              <input type='radio' name='oneway-trip' />
+              One-Way Trip
+            </label>
+          </div>
+
+          <div className='column'>
+            <label className='radio'>
+              <input type='radio' name='oneway-trip' />
+              Multi-Trip
+            </label>
+          </div>
         </div>
       </div>
     )
